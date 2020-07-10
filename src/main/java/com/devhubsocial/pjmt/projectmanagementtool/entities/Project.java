5b1b0bf9f918f84f9244f6e30f3a8452be4e0a4c
stevenmcdonald <engineers2526@gmail.com>
2020-07-10 13:31:30 -0500

    Initial commit added project entity.

---
+++ src/main/java/com/devhubsocial/pjmt/projectmanagementtool/entities/Project.java
@@ -0,0 +1,30 @@
+package com.devhubsocial.pjmt.projectmanagementtool.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@NoArgsConstructor
+@Getter
+@Setter
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long projectId;
+    private String name;
+    private String stage;
+    private String description;
+
+    public Project(String name, String stage, String description) {
+        this.name = name;
+        this.stage = stage;
+        this.description = description;
+    }
+}
